9efe491768c742ce6781deaf12617262689a1189
Gabriel Malet <gabrielmalet@gmail.com>
2014-06-07 18:29:22 +0200

    Factorisation + factorisation

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -2,13 +2,40 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-    .controller('HeaderCtrl', ['$scope', function ($scope) {
-        $scope.header = {name: "header.html", url: "partials/header.html"};
-    }])
-    .controller('MyCtrl1', ['$scope', function ($scope) {
+var tmsControllers = angular.module('tms.controllers', []);
 
-    }])
-    .controller('MyCtrl2', ['$scope', function ($scope) {
+tmsControllers.controller('IncludeCtrl', ['$scope',
+    function($scope) {
+        $scope.header = {name: "include.html", url: "partials/layout/include.html"};
+    }
+]);
 
-    }]);
+tmsControllers.controller('HeaderCtrl', ['$scope',
+    function($scope) {
+        $scope.header = {name: "header.html", url: "partials/layout/header.html"};
+    }
+]);
+
+tmsControllers.controller('FooterCtrl', ['$scope',
+    function($scope) {
+        $scope.footer = {name: "footer.html", url: "partials/layout/footer.html"};
+    }
+]);
+
+tmsControllers.controller('IncludeCtrl', ['$scope',
+    function($scope) {
+        $scope.footer = {name: "include.html", url: "partials/layout/include.html"};
+    }
+]);
+
+tmsControllers.controller('MyCtrl1', ['$scope',
+    function($scope) {
+        // todo : implement
+    }
+]);
+
+tmsControllers.controller('MyCtrl2', ['$scope',
+    function($scope) {
+        // todo : implement
+    }
+]);
